837e9a53eea62e74def7bac2d8d5da62ff748ee3
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-12-05 15:05:19 +0100

    adds another middleware

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ mongoose
 
 //app.options('*', cors());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
     'https://record-shop-frontend-zcy0.onrender.com'
@@ -76,6 +76,25 @@ app.use((req, res, next) => {
   ]);
   res.header('Access-Control-Allow-Headers', ['Content-Type', 'Authorization']);
   res.header('Access-Control-Expose-Headers', 'token');
+  next();
+}); */
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    'http://localhost:8000',
+    'https://record-shop-frontend-zcy0.onrender.com',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, token'
+  );
+  res.header('Access-Control-Allow-credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
   next();
 });
 
